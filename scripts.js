7f917ec594a9284031b4a79afd369af952dcb6d1
Moringa School Access <shebaz3000@gmail.com>
2020-08-26 18:58:15 +0300

    added bootstrap,linked my css,scripts to project

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-var add = function(number1, number2) {
+var add = function (number1, number2) {
   return number1 + number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+
+var subtact = function (number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function (number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function (number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function () {
+  $("form#add").submit(function () {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+});
